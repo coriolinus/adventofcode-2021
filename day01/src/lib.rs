bd932138a27850a5fffdc4d8ab58de2cb3e78271
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-12-01 08:44:54 +0100

    day01 simplify

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -2,22 +2,24 @@ use aoclib::parse;
 use itertools::Itertools;
 use std::path::Path;
 
-pub fn part1(input: &Path) -> Result<(), Error> {
-    let increases = parse::<u32>(input)?
-        .tuple_windows::<(_, _)>()
+fn count_increases(iter: impl Iterator<Item = u32>) -> usize {
+    iter.tuple_windows::<(_, _)>()
         .filter(|(a, b)| b > a)
-        .count();
+        .count()
+}
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    let increases = count_increases(parse::<u32>(input)?);
     println!("increases: {}", increases);
     Ok(())
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    let increases = parse::<u32>(input)?
-        .tuple_windows::<(_, _, _)>()
-        .map(|(a, b, c)| a + b + c)
-        .tuple_windows::<(_, _)>()
-        .filter(|(a, b)| b > a)
-        .count();
+    let increases = count_increases(
+        parse::<u32>(input)?
+            .tuple_windows::<(_, _, _)>()
+            .map(|(a, b, c)| a + b + c),
+    );
     println!("increases (3-windows): {}", increases);
     Ok(())
 }
@@ -26,6 +28,4 @@ pub fn part2(input: &Path) -> Result<(), Error> {
 pub enum Error {
     #[error(transparent)]
     Io(#[from] std::io::Error),
-    #[error("No solution found")]
-    NoSolution,
 }
