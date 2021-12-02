cac1b8637ea9203d5f8ed815f0f6af14d662b492
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-12-02 10:04:06 +0100

    refactor count_increases as iterator extension
    This somewhat increases the complexity of the implementation, but it
    makes it much easier to read and understand the code where it's actually
    used; it's just the terminus of a method chain, instead of a function
    capturing a method chain as its argument.

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -2,24 +2,33 @@ use aoclib::parse;
 use itertools::Itertools;
 use std::path::Path;
 
-fn count_increases(iter: impl Iterator<Item = u32>) -> usize {
-    iter.tuple_windows::<(_, _)>()
-        .filter(|(a, b)| b > a)
-        .count()
+trait CountIncreases {
+    fn count_increases(self) -> usize;
+}
+
+impl<Iter, Item> CountIncreases for Iter
+where
+    Iter: Iterator<Item = Item>,
+    Item: PartialOrd + Clone,
+{
+    fn count_increases(self) -> usize {
+        self.tuple_windows::<(_, _)>()
+            .filter(|(a, b)| b > a)
+            .count()
+    }
 }
 
 pub fn part1(input: &Path) -> Result<(), Error> {
-    let increases = count_increases(parse::<u32>(input)?);
+    let increases = parse::<u32>(input)?.count_increases();
     println!("increases: {}", increases);
     Ok(())
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    let increases = count_increases(
-        parse::<u32>(input)?
-            .tuple_windows::<(_, _, _)>()
-            .map(|(a, b, c)| a + b + c),
-    );
+    let increases = parse::<u32>(input)?
+        .tuple_windows::<(_, _, _)>()
+        .map(|(a, b, c)| a + b + c)
+        .count_increases();
     println!("increases (3-windows): {}", increases);
     Ok(())
 }
