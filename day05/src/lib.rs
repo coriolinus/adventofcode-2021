f1d07b7f65a6ec53941fdde163c2970db978b10b
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-12-05 16:08:42 +0100

    day05 part 1

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,7 +1,12 @@
-use aoclib::parse;
+use aoclib::{
+    geometry::{line::Line, Point},
+    parse,
+};
 use std::path::Path;
 
-#[derive(Debug, parse_display::FromStr, parse_display::Display)]
+const MAP_EDGE: usize = 1024;
+
+#[derive(Debug, parse_display::FromStr, parse_display::Display, Clone, Copy)]
 #[display("{x1},{y1} -> {x2}{y2}")]
 #[from_str(regex = r"(?P<x1>\d+),(?P<y1>\d+)\s+->\s+(?P<x2>\d+),(?P<y2>\d+)")]
 struct VentLine {
@@ -11,28 +16,64 @@ struct VentLine {
     y2: usize,
 }
 
-pub fn part1(input: &Path) -> Result<(), Error> {
-    let mut x_min = !0;
-    let mut y_min = !0;
-    let mut x_max = 0;
-    let mut y_max = 0;
+impl From<VentLine> for Line {
+    fn from(vl: VentLine) -> Self {
+        Line::new(
+            Point::new(vl.x1 as i32, vl.y1 as i32),
+            Point::new(vl.x2 as i32, vl.y2 as i32),
+        )
+    }
+}
 
-    let mut count = 0;
+fn is_horizontal_or_vertical(line: Line) -> bool {
+    line.from.x == line.to.x || line.from.y == line.to.y
+}
 
-    for vent_line in parse::<VentLine>(input)? {
-        x_min = x_min.min(vent_line.x1);
-        x_min = x_min.min(vent_line.x2);
-        x_max = x_max.max(vent_line.x1);
-        x_max = x_max.max(vent_line.x2);
-        y_min = y_min.min(vent_line.y1);
-        y_min = y_min.min(vent_line.y2);
-        y_max = y_max.max(vent_line.y1);
-        y_max = y_max.max(vent_line.y2);
-
-        count += 1;
+/// Iterate over the points of the line, inclusive.
+///
+/// Only works for horizontal or vertical lines.
+///
+/// Consider adding this to aoclib.
+fn line_points<'a>(line: Line) -> Option<impl Iterator<Item = Point>> {
+    if !is_horizontal_or_vertical(line) {
+        return None;
     }
 
-    dbg!(x_min, x_max, y_min, y_max, count);
+    let vector = line.to - line.from;
+    let dx = vector.x / vector.x.abs().max(1);
+    let dy = vector.y / vector.y.abs().max(1);
+
+    Some(
+        std::iter::successors(Some(line.from), move |prev| {
+            if *prev == line.to {
+                None
+            } else {
+                let mut next = *prev;
+                next.x += dx;
+                next.y += dy;
+                Some(next)
+            }
+        })
+        .fuse(),
+    )
+}
+
+type Map = aoclib::geometry::Map<u8>;
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    let mut map = Map::new(MAP_EDGE, MAP_EDGE);
+    for point in parse::<VentLine>(input)?
+        .map(Into::into)
+        .filter_map(line_points)
+        .flatten()
+    {
+        map[point] += 1;
+    }
+
+    let intersections_count = map.iter().filter(|(_, count)| **count > 1).count();
+
+    println!("count of intersections: {}", intersections_count);
+
     Ok(())
 }
 
