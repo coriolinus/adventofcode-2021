3d29fc463c0b0de0aa662188795e3010b2d02e87
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-12-07 14:39:03 +0100

    experiment with parallelism in day 7
    It's not worth it for a problem of this size. For my input, the
    single threaded version was substantially faster for both parts.
    Unsurprisingly though, the parallel version performed better when
    more computation was required in part 2. Perhaps a hypothetical
    part 3 would actually benefit from parallelism.
    
    $ hyperfine --warmup 10 './day07_single' './day07_parallel'
    Tue 07 Dec 2021 02:37:15 PM CET
    Benchmark #1: ./day07_single
      Time (mean ± σ):       1.2 ms ±   0.1 ms    [User: 1.1 ms, System: 0.1 ms]
      Range (min … max):     1.1 ms …   3.3 ms    1870 runs
    
      Warning: Command took less than 5 ms to complete. Results might be inaccurate.
      Warning: Statistical outliers were detected. Consider re-running this benchmark on a quiet PC without any interferences from other programs. It might help to use the '--warmup' or '--prepare' options.
    
    Benchmark #2: ./day07_parallel
      Time (mean ± σ):       2.6 ms ±   0.4 ms    [User: 4.4 ms, System: 0.9 ms]
      Range (min … max):     1.6 ms …   5.4 ms    994 runs
    
      Warning: Command took less than 5 ms to complete. Results might be inaccurate.
    
    Summary
      './day07_single' ran
        2.16 ± 0.40 times faster than './day07_parallel'
    coriolinus@brute ~/projects/coriolinus/adventofcode-2021
    $ hyperfine --warmup 10 './day07_single --part2 --no-part1' './day07_parallel --part2 --no-part1'
    Tue 07 Dec 2021 02:37:47 PM CET
    Benchmark #1: ./day07_single --part2 --no-part1
      Time (mean ± σ):       1.7 ms ±   0.1 ms    [User: 1.6 ms, System: 0.1 ms]
      Range (min … max):     1.5 ms …   2.3 ms    1435 runs
    
      Warning: Command took less than 5 ms to complete. Results might be inaccurate.
      Warning: Statistical outliers were detected. Consider re-running this benchmark on a quiet PC without any interferences from other programs. It might help to use the '--warmup' or '--prepare' options.
    
    Benchmark #2: ./day07_parallel --part2 --no-part1
      Time (mean ± σ):       2.6 ms ±   0.4 ms    [User: 4.8 ms, System: 1.0 ms]
      Range (min … max):     1.6 ms …   4.0 ms    906 runs
    
      Warning: Command took less than 5 ms to complete. Results might be inaccurate.
    
    Summary
      './day07_single --part2 --no-part1' ran
        1.56 ± 0.27 times faster than './day07_parallel --part2 --no-part1'

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,18 +1,26 @@
 use aoclib::{parse, CommaSep};
 use std::path::Path;
 
+#[cfg(feature = "parallelism")]
+use rayon::prelude::*;
+
 fn total_fuel_at_best_position(
     crab_submarines: &[i32],
-    per_submarine: impl Fn(i32, i32) -> i32,
+    fuel_per_submarine: impl Sync + Fn(i32) -> i32,
 ) -> Option<i32> {
     let min = *crab_submarines.iter().min()?;
     let max = *crab_submarines.iter().max()?;
-    (min..=max)
+    #[cfg(not(feature = "parallelism"))]
+    let range = min..=max;
+    #[cfg(feature = "parallelism")]
+    let range = (min..=max).into_par_iter();
+
+    range
         .map(|assembly_point| {
             crab_submarines
                 .iter()
                 .copied()
-                .map(|submarine| per_submarine(submarine, assembly_point))
+                .map(|submarine| fuel_per_submarine((submarine - assembly_point).abs()))
                 .sum::<i32>()
         })
         .min()
@@ -25,11 +33,15 @@ fn triangular_sequence(n: i32) -> i32 {
     n * (n + 1) / 2
 }
 
-fn solve(input: &Path, nature: &str, per_submarine: impl Fn(i32, i32) -> i32) -> Result<(), Error> {
+fn solve(
+    input: &Path,
+    nature: &str,
+    fuel_per_submarine: impl Sync + Fn(i32) -> i32,
+) -> Result<(), Error> {
     for (idx, line) in parse::<CommaSep<i32>>(input)?.enumerate() {
         let crab_submarines: Vec<_> = line.into();
         let total_fuel_at_best_position =
-            total_fuel_at_best_position(&crab_submarines, &per_submarine)
+            total_fuel_at_best_position(&crab_submarines, &fuel_per_submarine)
                 .ok_or(Error::NoSolution)?;
         println!(
             "{}: total fuel at best position ({}): {}",
@@ -40,16 +52,11 @@ fn solve(input: &Path, nature: &str, per_submarine: impl Fn(i32, i32) -> i32) ->
 }
 
 pub fn part1(input: &Path) -> Result<(), Error> {
-    solve(input, "linear", |submarine, assembly_point| {
-        (submarine - assembly_point).abs()
-    })
+    solve(input, "linear", std::convert::identity)
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    solve(input, "increasing rate", |submarine, assembly_point| {
-        let n = (submarine - assembly_point).abs();
-        triangular_sequence(n)
-    })
+    solve(input, "increasing rate", triangular_sequence)
 }
 
 #[derive(Debug, thiserror::Error)]
