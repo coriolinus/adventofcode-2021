d60bcf8f6b11f53caf5970e25365618bf230d7aa
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-12-09 13:21:20 +0100

    day09 part 2
    It came in _really_ handy to have a good map library today.
    Challenge mode: there was a bug in the library causing overflow;
    that was weird. Not too hard to sort out ultimately.
    
    This day looks nicely compact!

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,20 +1,54 @@
-use aoclib::geometry::map::tile::Digit;
+use aoclib::geometry::{
+    map::{ContextInto, Traversable},
+    tile::DisplayWidth,
+    Point,
+};
 use std::path::Path;
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, derive_more::FromStr)]
+struct Digit(aoclib::geometry::map::tile::Digit);
+
+impl DisplayWidth for Digit {
+    const DISPLAY_WIDTH: usize = aoclib::geometry::map::tile::Digit::DISPLAY_WIDTH;
+}
+
+impl From<Digit> for u8 {
+    fn from(digit: Digit) -> u8 {
+        digit.0.into()
+    }
+}
+
+impl ContextInto<Traversable> for Digit {
+    type Context = ();
+
+    fn ctx_into(self, _position: Point, _context: &Self::Context) -> Traversable {
+        match self.into() {
+            9 => Traversable::Obstructed,
+            _ => Traversable::Free,
+        }
+    }
+}
 
 type Map = aoclib::geometry::Map<Digit>;
 
-pub fn part1(input: &Path) -> Result<(), Error> {
+fn read_input(input: &Path) -> Result<(Map, Vec<Point>), Error> {
     let map = <Map as TryFrom<&Path>>::try_from(input)?;
-    let low_points: Vec<_> = map
+    let low_points = map
         .iter()
         .filter(|(point, height)| {
             map.orthogonal_adjacencies(*point)
                 .all(|adj| map[adj] > **height)
         })
+        .map(|(point, _)| point)
         .collect();
+    Ok((map, low_points))
+}
+
+pub fn part1(input: &Path) -> Result<(), Error> {
+    let (map, low_points) = read_input(input)?;
     let risk_level = low_points
         .iter()
-        .map(|(_, digit)| <Digit as Into<u8>>::into(**digit) as u32 + 1)
+        .map(|point| <Digit as Into<u8>>::into(map[*point]) as u32 + 1)
         .sum::<u32>();
 
     println!("sum of low point risk levels: {}", risk_level);
@@ -22,7 +56,24 @@ pub fn part1(input: &Path) -> Result<(), Error> {
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    unimplemented!("input file: {:?}", input)
+    let (map, low_points) = read_input(input)?;
+    let mut region_sizes: Vec<_> = low_points
+        .iter()
+        .map(|point| {
+            let mut size: u64 = 0;
+            map.reachable_from(*point, |_point, _tile| {
+                size += 1;
+                false
+            });
+            size
+        })
+        .collect();
+    region_sizes.sort_unstable();
+    let basin_size_product: u64 = region_sizes.iter().rev().take(3).product();
+
+    println!("product of 3 largest basin sizes: {}", basin_size_product);
+
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -31,6 +82,4 @@ pub enum Error {
     Io(#[from] std::io::Error),
     #[error("could not read map")]
     MapConv(#[from] aoclib::geometry::map::MapConversionErr),
-    #[error("no solution found")]
-    NoSolution,
 }
