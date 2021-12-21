e93be66b6ac9294c8d1aefcb740274c90d47bed1
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-12-21 22:55:47 +0100

    day19: more setup work

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -1,4 +1,5 @@
 use aoclib::{geometry::vector3::Vector3, input::parse_newline_sep};
+use enum_iterator::IntoEnumIterator;
 use std::{
     io::{BufRead, Cursor},
     path::Path,
@@ -27,10 +28,72 @@ impl From<Vector3Parse> for Vector3 {
 #[display("--- scanner {0} ---")]
 struct ScannerHeader(u32);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, IntoEnumIterator)]
+enum Axis {
+    X,
+    Y,
+    Z,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, IntoEnumIterator)]
+enum Negation {
+    Positive,
+    Negative,
+}
+
+/// An `Orientation` is a transform from one labeled coordinate system to another.
+///
+/// Each coordinate system is assumed to share the same orthogonal axes; the only
+/// difference is the label and direction of positive assigned to each axis.
+///
+/// There are 48 possible orientations: 6 permutations of axis labels multiplied
+/// by 8 possible axis-positive assignments.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Orientation([(Negation, Axis); 3]);
+
+impl Orientation {
+    fn exhaustive_iterator() -> impl Iterator<Item = Orientation> {
+        todo!();
+        std::iter::empty()
+    }
+
+    fn extract(point: Vector3, (negation, axis): (Negation, Axis)) -> i32 {
+        let negation = match negation {
+            Negation::Positive => 1,
+            Negation::Negative => -1,
+        };
+        let value = match axis {
+            Axis::X => point.x,
+            Axis::Y => point.y,
+            Axis::Z => point.z,
+        };
+        negation * value
+    }
+
+    fn transform(self, point: Vector3) -> Vector3 {
+        Vector3 {
+            x: Self::extract(point, self.0[0]),
+            y: Self::extract(point, self.0[1]),
+            z: Self::extract(point, self.0[2]),
+        }
+    }
+}
+
+impl Default for Orientation {
+    fn default() -> Self {
+        use Axis::*;
+        use Negation::Positive;
+
+        Self([(Positive, X), (Positive, Y), (Positive, Z)])
+    }
+}
+
+#[derive(Debug, Clone, Default)]
 struct Scanner {
     id: u32,
     beacons: Vec<Vector3>,
+    absolute_position: Option<Vector3>,
+    orientation: Option<Orientation>,
 }
 
 impl FromStr for Scanner {
@@ -53,6 +116,7 @@ impl FromStr for Scanner {
         Ok(Scanner {
             id: header.0,
             beacons,
+            ..Scanner::default()
         })
     }
 }
